3e07a8edb8fd703353eec74527d4917d098a0479
Buckwich <Buckwich@users.noreply.github.com>
2020-12-13 00:34:02 +0100

    add stats

--- stats/stats.py
+++ stats/stats.py
@@ -2,8 +2,21 @@ import csv
 import json
 from pathlib import Path
 
+total_keymap_count = 0
+total_no_cpp_count = 0
+total_error_count = 0
+total_keymaps = 0
+total_keymaps_without_info_json = 0
+
 
 def toRow(keyboard):
+    global total_keymap_count
+    global total_keymap_count
+    global total_no_cpp_count
+    global total_error_count
+    global total_keymaps
+    global total_keymaps_without_info_json
+
     if(not (Path("../keymaps/"+keyboard["name"]+"/keymaps.json").exists())):
         return[]
     keymaps_json = open("../keymaps/"+keyboard["name"]+"/keymaps.json")
@@ -18,7 +31,12 @@ def toRow(keyboard):
             no_cpp_count = no_cpp_count+1
         if("error" in keymap):
             error_count = error_count+1
-
+    total_keymap_count = total_keymap_count+keymap_count
+    total_no_cpp_count = total_no_cpp_count+no_cpp_count
+    total_error_count = total_error_count+error_count
+    total_keymaps = total_keymaps+1
+    if keyboard["path"] is None:
+        total_keymaps_without_info_json = total_keymaps_without_info_json+1
     return [keyboard["name"], keyboard["path"], keymap_count, no_cpp_count, error_count]
 
 
@@ -37,3 +55,8 @@ with open('stats.csv', 'w', newline='') as csvfile:
     for keyboard in keyboards:
         row = toRow(keyboard)
         spamwriter.writerow(row)
+print("total_keymaps: "+str(total_keymaps))
+print("total_keymaps_without_info_json: "+str(total_keymaps_without_info_json))
+print("total_keymap_count: "+str(total_keymap_count))
+print("total_no_cpp_count: "+str(total_no_cpp_count))
+print("total_error_count: "+str(total_error_count))
